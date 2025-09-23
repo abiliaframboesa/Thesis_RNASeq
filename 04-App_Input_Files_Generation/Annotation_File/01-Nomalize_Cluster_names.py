65898a0101f8a9e0413b28af2a2ef025d0f043bd
abiliaframboesa <152984261+abiliaframboesa@users.noreply.github.com>
2025-09-23 22:37:29 +0100

    Update 01-Nomalize_Cluster_names.py

--- 04-App_Input_Files_Generation/Annotation_File/01-Nomalize_Cluster_names.py
+++ 04-App_Input_Files_Generation/Annotation_File/01-Nomalize_Cluster_names.py
@@ -4,10 +4,14 @@
 import pandas as pd
 import re
 
-# Lê o ficheiro XLSX 
+# 1. Lê o ficheiro XLSX
 df = pd.read_excel("eggnog_final_anotado.xlsx")  
 
-# Função para converter IDs
+# 2. Se a coluna que contém os clusters se chama 'Cluster', renomeia para 'gene'
+if 'Cluster' in df.columns:
+    df = df.rename(columns={'Cluster': 'gene'})
+
+# 3. Função para converter IDs
 def converter_id(gene_id):
     match = re.match(r"(Cluster)_(\d+)_([0-9]+)\.p[0-9]+", str(gene_id))
     if match:
@@ -15,13 +19,14 @@ def converter_id(gene_id):
         return f"{cluster}-{numero}.{sub}"
     return gene_id
 
-# Aplica a conversão à coluna 'gene'
+# 4. Aplica a conversão à coluna 'gene'
 df['gene'] = df['gene'].apply(converter_id)
 
-# Salva de volta para XLSX 
+# 5. Salva de volta para XLSX 
 df.to_excel("anotacoes_convertidas.xlsx", index=False)
 # df.to_csv("anotacoes_convertidas.csv", sep='\t', index=False)
 
 print("Conversão concluída com sucesso!")
+
 
 
